1377b42cf0896367566128b96f9f39345760caf6
bizzy <hopebiziyaremye@gmail.com>
2022-09-09 00:10:26 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
-kdk
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  * takes no args
+  *
+  * Description: used printf(); function to print to stdout
+  * Return: Program always return 0(success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
