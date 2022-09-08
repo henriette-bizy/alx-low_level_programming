18e2382416ed3c5f518b835fb65a8c3db7888ed1
henriette-bizy <hopebiziyaremye@gmail.com>
2022-09-08 14:25:02 -0700

    add c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point of the program
+ *
+ *   * takes no arg.
+ *
+ *    *
+ *
+ *     * Des: used puts() func to print string to stdout
+ *
+ *      * Return: function returns 0 always(success)
+ *
+ *       */
+
+int main(void) /*main func*/
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
