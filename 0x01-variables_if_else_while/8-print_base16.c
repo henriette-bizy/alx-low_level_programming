4229509c770c06057b618a477ef3e5c6d3ac2165
bizzy <hopebiziyaremye@gmail.com>
2022-09-10 00:25:56 +0200

    update 8-print-base.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,25 @@
-hddd
+#include <stdio.h>
+
+/**
+  * main - entry point of the program
+  *
+  * Takes no arguments
+  *
+  * Return: program always returns 0(success)
+  */
+int main(void)
+{
+	char nbase_16;
+
+	for (nbase_16 = 48; nbase_16 <= 57; nbase_16++)
+	{
+		putchar(nbase_16);
+	}
+	for (nbase_16 = 97; nbase_16 <= 102; nbase_16++)
+	{
+		putchar(nbase_16);
+	}
+	putchar('\n');
+
+	return (0);
+}
