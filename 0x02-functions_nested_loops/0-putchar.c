1719ea9e489fab62da559890ca2b364624cd6847
bizzy <hopebiziyaremye@gmail.com>
2022-09-13 21:25:17 +0200

    Updates on putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,13 @@
-jdjdjdj
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - Entry point of the program"
+  *
+  * Return: Always returns 0(success)
+  */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
