436a67cb7380c3a52579d6566fa0ccd98d339c92
bizzy <hopebiziyaremye@gmail.com>
2022-09-22 20:27:28 +0200

    udpate 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
+	{
+		if (s1[j] != s2[j])
+			return (s1[j] - s2[j]);
+	}
+	return (0);
+}
