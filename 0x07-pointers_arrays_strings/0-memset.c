5c0361ee52bb2f4f5d5654b975c2938ea10a27ab
henriette-bizy <hopebiziyaremye@gmail.com>
2022-09-26 12:00:58 -0700

    betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
+ * _memset: function to write constansts on some reserved bytes
  * @s: pointer holding the memory
  * @b:the constant byte to fill the constant
  * @n: number of bytes to fill the constant
- * */
+ * Return : *s
+ */
 
- char *_memset(char *s, char b, unsigned int n ){
+ char *_memset(char *s, char b, unsigned int n )
+{
  
-unsigned int i;
-for(i = 0, i<n,i++){
-   s[i] = b;
+       unsigned int i;
 
-}
-return (s);
+      for(i = 0, i<n,i++)
+      {
+          s[i] = b;
+
+      }
+
+      return (s);
  
  
  }
