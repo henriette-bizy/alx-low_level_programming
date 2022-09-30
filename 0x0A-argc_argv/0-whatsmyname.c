27e525ae951e236450ecc66bee5eb8c3c5768295
bizzy <hopebiziyaremye@gmail.com>
2022-09-30 17:17:58 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - prints its name, followed by a new line.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * Return: 0 - success
  */
 int main(int argc _attribute_((unused)), char *argv[])
 {
